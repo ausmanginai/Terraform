f17170dfa77221a8898980bf19598e52f45df60b
Ausman Ginai <ausmanginai.ericsson@gmail.com>
2025-03-03 15:52:20 +0000

    ready to deploy ec2 server

--- main.tf
+++ main.tf
@@ -1,31 +1,117 @@
 
-provider "aws" {}
-
-
-variable "cidr_blocks" {
-    description = "subnet cidr block"
-    type = list(string)
+provider "aws" {
+    region = "eu-west-2"
 }
 
-variable "environment" {
-    description= "deployment environment"
-}
 
-variable availability_zone{}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable availability_zone {}
+variable env_prefix {} // can be either dev, prod or staging etc. 
+variable my_ip {}
+variable instance_type {}
 
-resource "aws_vpc" "development-vpc" { // aws_vpc is 'provider_resource' and development-vpc is the resource like a variable name
-    cidr_block = var.cidr_blocks[0]
+
+resource "aws_vpc" "myapp-vpc" { // aws_vpc is 'provider_resource' and development-vpc is the 
+    // resource like a variable name
+    cidr_block = var.vpc_cidr_block
     tags = {
-        Name: var.environment
+        Name: "${var.env_prefix}-vpc" // as the variable is being used inside a string, 
+        // it has a different script with brackets and $
     }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-    vpc_id = aws_vpc.development-vpc.id // as the vpc above doesnt exist yet, so use resource.variable.id to get its id
-    cidr_block = var.cidr_blocks[1]
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id // as the vpc above doesnt exist yet, so use resource.variable.id to get its id
+    cidr_block = var.subnet_cidr_block
     availability_zone = var.availability_zone// region has 3 AZs, so just chose one
     tags = {
-        Name: "subnet-1-dev"
+        Name: "${var.env_prefix}-subnet"
     }
 }
 
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+        Name = "${var.env_prefix}-igw"
+    }
+}
+
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+        Name: "${var.env_prefix}-main-rtb"
+    }
+
+}
+
+resource "aws_default_security_group" "default-sg" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress { // for SSH
+        from_port = 22
+        to_port = 22
+        protocol = "TCP"
+        cidr_blocks = [var.my_ip]
+    }
+
+    ingress { // for NGINX server
+        from_port = 8080
+        to_port = 8080
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress { // all attributes configured to any/everything
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+
+    }
+
+    tags = {
+        Name: "${var.env_prefix}-default-sg"
+    }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent =  true
+    owners = ["amazon"]
+    filter {
+        name = "name" // name of parameter to filter with
+        values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.availability_zone
+
+    associate_public_ip_address = true
+    key_name = "server-key-pair-terraform"
+
+    tags = {
+        Name: "${var.env_prefix}-server"
+    }
+}
+
+
+output "aws_ami_id" {
+    value = data.aws_ami.latest-amazon-linux-image.id
+}
